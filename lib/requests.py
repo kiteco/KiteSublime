9743fe16799eb87f13375d7e9e30d059d1b21c61
Daniel Hung <daniel@kite.com>
2018-07-19 16:15:30 -0700

    reuse connection

--- lib/requests.py
+++ lib/requests.py
@@ -5,12 +5,17 @@ from ..lib import logger
 
 _KITED_HOST = 'localhost'
 _KITED_PORT = 46624
+_conn = None
 
 def kited_post(path, data=None):
-    conn = HTTPConnection(_KITED_HOST, port=_KITED_PORT)
-    conn.request('POST', path,
-                 body=(json.dumps(data) if data is not None else None))
-    resp = conn.getresponse()
+    if _conn is None:
+        _init_connection()
+    _conn.request('POST', path, headers={'Connection': 'keep-alive'},
+                  body=(json.dumps(data) if data is not None else None))
+    resp = _conn.getresponse()
     body = resp.read()
-    conn.close()
     return resp, body
+
+def _init_connection():
+    global _conn
+    _conn = HTTPConnection(_KITED_HOST, port=_KITED_PORT)
