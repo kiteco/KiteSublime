0310b4e092ebf3301d854b60202882baa5163be3
Daniel Hung <dhung09@gmail.com>
2018-08-19 16:26:21 -0700

    update comments

--- KPP.py
+++ KPP.py
@@ -8,14 +8,16 @@ from .lib.listeners import *
 _consumer = None
 
 def plugin_loaded():
-    """Called when the plugin is first loaded
+    """Called when the plugin is first loaded. Instantiates a single consumer
+    instance to handle deferred events.
     """
     global _consumer
     _consumer = deferred.consume()
     logger.log('KPP activated')
 
 def plugin_unloaded():
-    """Called before the plugin is unloaded
+    """Called before the plugin is unloaded. Stops the consumer immediately
+    without waiting for the queue to be empty.
     """
     if _consumer:
         _consumer.stop()
