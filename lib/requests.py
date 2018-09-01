71b3979ca9d26226cb936f7828966eae101b1148
Daniel Hung <dhung09@gmail.com>
2018-08-31 23:51:27 -0700

    use a pool of connections

--- lib/requests.py
+++ lib/requests.py
@@ -1,46 +1,51 @@
 import json
+import random
 from http.client import CannotSendRequest, HTTPConnection
 
 _KITED_HOST = 'localhost'
 _KITED_PORT = 46624
-_conn = None
+_conns = [None]*4
 
 
 def kited_get(path):
-    if _conn is None:
-        _init_connection()
+    conn, idx = _get_connection()
 
     try:
-        _conn.request('GET', path, headers={'Connection': 'keep-alive'})
+        conn.request('GET', path, headers={'Connection': 'keep-alive'})
     except (ConnectionRefusedError, CannotSendRequest) as ex:
-        _reset_connection()
+        _reset_connection(idx)
         raise ex
     else:
-        resp = _conn.getresponse()
+        resp = conn.getresponse()
         body = resp.read()
         return resp, body
 
 def kited_post(path, data=None):
-    if _conn is None:
-        _init_connection()
+    conn, idx = _get_connection()
 
     try:
-        _conn.request('POST', path, headers={'Connection': 'keep-alive'},
-                      body=(json.dumps(data) if data is not None else None))
+        conn.request('POST', path, headers={'Connection': 'keep-alive'},
+                     body=(json.dumps(data) if data is not None else None))
     except (ConnectionRefusedError, CannotSendRequest) as ex:
-        _reset_connection()
+        _reset_connection(idx)
         raise ex
     else:
-        resp = _conn.getresponse()
+        resp = conn.getresponse()
         body = resp.read()
         return resp, body
 
 
-def _init_connection():
-    global _conn
-    _conn = HTTPConnection(_KITED_HOST, port=_KITED_PORT, timeout=0.25)
+def _get_connection():
+    idx = random.randint(0, 3)
+    if _conns[idx] is None:
+        _init_connection(idx)
+    return _conns[idx], idx
 
-def _reset_connection():
-    global _conn
-    _conn.close()
-    _conn = None
+def _init_connection(idx):
+    global _conns
+    _conns[idx] = HTTPConnection(_KITED_HOST, port=_KITED_PORT, timeout=0.25)
+
+def _reset_connection(idx):
+    global _conns
+    _conns[idx].close()
+    _conns[idx] = None
