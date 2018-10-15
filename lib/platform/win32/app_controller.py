0e2d6846caf2992bbbb94623fd7a3552f70ad48a
Daniel Hung <daniel@kite.com>
2018-10-15 09:24:45 -0700

    convert tabs to spaces

--- lib/platform/win32/app_controller.py
+++ lib/platform/win32/app_controller.py
@@ -5,21 +5,21 @@ __all__ = ['_launch_kite', '_locate_kite']
 _QUERY = 'reg query "HKEY_LOCAL_MACHINE\\Software\\Kite\\AppData" /v InstallPath /s /reg:64'
 
 def _launch_kite(app):
-	proc = subprocess.Popen([app])
-	return proc
+    proc = subprocess.Popen([app])
+    return proc
 
 def _locate_kite():
-	installed = None
-	app = None
+    installed = None
+    app = None
 
-	try:
-		out = subprocess.check_output(_QUERY)
-		installed = len(out) > 0
-		if installed:
-			res = out.decode().strip().split('\n')[1].strip()
-			app = '{}\\kited.exe'.format(res[res.find('C:\\'):])
-	except subprocess.CalledProcessError:
-		installed = False
-		app = None
-	finally:
-		return (installed, app)
+    try:
+        out = subprocess.check_output(_QUERY)
+        installed = len(out) > 0
+        if installed:
+            res = out.decode().strip().split('\n')[1].strip()
+            app = '{}\\kited.exe'.format(res[res.find('C:\\'):])
+    except subprocess.CalledProcessError:
+        installed = False
+        app = None
+    finally:
+        return (installed, app)
