b248fdc5cda34d277b4a2944bc6948ef5254568d
Daniel Hung <dhung09@gmail.com>
2020-08-19 14:01:12 -0700

    check for kite installation first

--- lib/installer.py
+++ lib/installer.py
@@ -4,11 +4,11 @@ from ..lib import link_opener
 
 def install_kite():
     res = sublime.ok_cancel_dialog(
-        'Kite Engine not installed\n\n' +
+        'Kite is missing dependencies\n\n' +
         'Kite requires the Kite Engine backend to provide completions and ' +
-        'documentation. Please install it to use Kite.',
+        'documentation. Please install it to use Kite.\n',
         ok_title='Install Kite'
     )
     if res:
         link_opener.open_browser_url(
-            'https://github.com/kiteco/KiteSublime#installing-the-kite-engine')
+            'https://kite.com/download/?utm_source=sublime-plugin')
