d1dea735abb804f00f28bbbc5354ff24696eae0c
Tony Cheang <tony@kite.com>
2020-09-24 14:34:20 -0700

    remove: unused

--- lib/languages.py
+++ lib/languages.py
@@ -12,19 +12,12 @@ class Extensions:
     JSX = ".jsx"
     VUE = ".vue"
 
-LEXICAL_EXTS = (Extensions.GO, Extensions.JS, Extensions.JSX, Extensions.VUE)
-
 SUPPORTED_EXTS_TO_LANG = {
     Extensions.PY: Languages.PYTHON,
     Extensions.GO: Languages.GO,
     Extensions.JS: Languages.JAVASCRIPT,
     Extensions.JSX: Languages.JAVASCRIPT,
     Extensions.VUE: Languages.JAVASCRIPT,
-}
-
-_LANG_TO_ENABLED_PATH = {
-    Languages.GO: "/clientapi/settings/kite_lexical_enabled",
-    Languages.JAVASCRIPT: "/clientapi/settings/kite_js_enabled",
 }
 
 def ext_to_lang(ext):
